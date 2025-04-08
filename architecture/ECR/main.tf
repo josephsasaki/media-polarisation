1ca69bbe1c9cc9c74a9e135a367152c50c4c4519
AntuFTW <antarikshpatel2002@gmail.com>
2025-04-08 12:36:30 +0100

    ecr for terraform

---
+++ architecture/ECR/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+# VPC data
+data "aws_vpc" "c16_vpc" {
+  id = var.vpc_id
+}
+
+# Create scraper ECR
+
+resource "aws_ecr_repository" "scraper-ecr" {
+  name                 = var.ecr_name_scraper
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Create email ECR
+
+resource "aws_ecr_repository" "email-ecr" {
+  name                 = var.ecr_name_email
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Create archive ECR
+
+resource "aws_ecr_repository" "archive-ecr" {
+  name                 = var.ecr_name_archive
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+# Create dashboard ECR
+
+resource "aws_ecr_repository" "dashboard-ecr" {
+  name                 = var.ecr_name_dashboard
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ architecture/ECR/terraform.tfvars
@@ -0,0 +1,1 @@
+vpc_id="vpc-0f7ba8057a52dd82d"
