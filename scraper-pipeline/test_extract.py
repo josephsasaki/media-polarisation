1ea403b5f62042961bcb160c2a7f7623b6a80aa1
Josh Allen <trainee.josh.allen@sigmalabs.co.uk>
2025-04-08 14:49:20 +0100

    initial test

--- scraper-pipeline/test_extract.py
+++ scraper-pipeline/test_extract.py
@@ -1,2 +1,54 @@
+from unittest.mock import patch, MagicMock
+
+from extract import GuardianRSSFeedExtractor
 import pytest
-from extract import GuardianRSSFeedExtractor, ExpressRSSFeedExtractor
+
+
+@pytest.fixture()
+def success_request():
+    with patch("requests.get") as mock_get:
+        mock_response = MagicMock()
+        mock_response.status_code = 200
+        mock_response.return_value = {"data": "valid response"}
+        mock_get.return_value = mock_response
+
+        yield mock_get
+
+
+@pytest.fixture()
+def failure_request():
+    with patch("requests.get") as mock_get:
+        mock_response = MagicMock()
+        mock_response.status_code = 404
+        mock_response.return_value = None
+        mock_get.return_value = mock_response
+
+        yield mock_get
+
+
+def test_guard_body_extractor_200(success_request):
+    guard = GuardianRSSFeedExtractor(["http://mockcom/"])
+    result = guard.body_extractor("http://mock.com/")
+    assert result.return_value == {"data": "valid response"}
+
+
+def test_express_body_extractor_200(success_request):
+    guard = GuardianRSSFeedExtractor(["http://mockcom/"])
+    result = guard.body_extractor("http://mock.com/")
+    assert result.return_value == {"data": "valid response"}
+
+
+def test_guard_body_extractor_404(failure_request, capsys):
+    guard = GuardianRSSFeedExtractor(["http://mockcom/"])
+    result = guard.body_extractor("http://mock.com/")
+    captured = capsys.readouterr()
+    assert result.return_value is None
+    assert "No articles found." in captured.out
+
+
+def test_express_body_extractor_404(failure_request, capsys):
+    guard = GuardianRSSFeedExtractor(["http://mockcom/"])
+    result = guard.body_extractor("http://mock.com/")
+    captured = capsys.readouterr()
+    assert result.return_value is None
+    assert "No articles found." in captured.out
