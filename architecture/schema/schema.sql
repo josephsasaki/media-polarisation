4be70b0ff7ca7146db78b92f258c4ac205a85ba4
AntuFTW <antarikshpatel2002@gmail.com>
2025-04-09 11:07:17 +0100

    changed seeding of news_outlet table and changed drop table order for idempotency

--- architecture/schema/schema.sql
+++ architecture/schema/schema.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS topic;
 DROP TABLE IF EXISTS article_topic;
 DROP TABLE IF EXISTS article;
 DROP TABLE IF EXISTS news_outlet;
+DROP TABLE IF EXISTS topic;
 
 
 CREATE TABLE topic (
