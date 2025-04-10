a1a0480ca29b7b6629f4442083210f58bb4879c2
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-10 21:50:28 +0100

    Refactored pipeline

--- scraper-pipeline/scraper.py
+++ scraper-pipeline/scraper.py
@@ -1,7 +1,9 @@
 
 
+from dotenv import load_dotenv
 from extract import GuardianRSSFeedExtractor, ExpressRSSFeedExtractor
-from transform import ArticleFactory, TextAnalyser
+from transform import ArticleFactory
+from analysis import TextAnalyser
 from load import DatabaseManager
 
 
@@ -17,22 +19,33 @@ class NewsScraper:
                 "https://www.express.co.uk/posts/rss/78/world",
             ]),
         ]
+        self.__db_manager = DatabaseManager()
+        self.__text_analyser = TextAnalyser(
+            valid_topics=self.__db_manager.get_valid_topics()
+        )
 
     def run(self):
         '''Run entire news scraper pipeline.'''
+        # EXTRACT
         all_articles = []
         for extractor in self.__rss_feed_extractors:
             feed = extractor.extract_feeds()
             all_articles.extend(feed)
-
-        article_factory = ArticleFactory(all_articles)
+        # TRANSFORM
+        article_factory = ArticleFactory(
+            raw_data=all_articles[0:2],
+            existing_urls=self.__db_manager.get_article_urls()
+        )
         articles = article_factory.generate_articles()
-
-        text_analyser = TextAnalyser(articles)
-        text_analyser.extract_topics()
-        text_analyser.perform_article_body_analysis()
-        text_analyser.perform_topic_analysis()
+        # ANALYSIS
+        self.__text_analyser.extract_topics(articles)
+        self.__text_analyser.perform_topic_analyses(articles)
+        self.__text_analyser.perform_body_analyses(articles)
+        # LOAD
+        self.__db_manager.insert_into_database(articles)
+        self.__db_manager.close_connection()
 
 
 if __name__ == "__main__":
+    load_dotenv(override=True)
     NewsScraper().run()
