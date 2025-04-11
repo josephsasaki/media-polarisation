59552d2065e8f411b4c3251b383becc852be6b03
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-11 11:32:57 +0100

    Progress

---
+++ scraper-pipeline/scraper-dispatcher/lambda_handler.py
@@ -0,0 +1,44 @@
+
+import os
+import boto3
+import json
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+LAMBDA_CLIENT = boto3.client(
+    "lambda",
+    aws_access_key_id=os.environ['ACCESS_KEY'],
+    aws_secret_access_key=os.environ['SECRET_ACCESS_KEY'],
+    region_name=os.environ['LAMBDA_REGION']
+)
+PAYLOADS = [
+    {
+        "guardian": [
+            "https://www.theguardian.com/politics/rss",
+            "https://www.theguardian.com/us-news/us-politics/rss",
+            "https://www.theguardian.com/world/rss"
+        ],
+        "express": []
+    },
+    {
+        "guardian": [],
+        "express": [
+            "https://www.express.co.uk/posts/rss/139/politics",
+            "https://www.express.co.uk/posts/rss/198/us",
+            "https://www.express.co.uk/posts/rss/78/world"
+        ]
+    }
+]
+
+
+def lambda_handler(event=None, context=None):
+    for payload in PAYLOADS:
+        LAMBDA_CLIENT.invoke(
+            FunctionName=os.environ['WORKER_FUNCTION_NAME'],
+            InvocationType="Event",  # async
+            Payload=json.dumps(payload)
+        )
+
+
+if __name__ == "__main__":
+    lambda_handler()
--- scraper-pipeline/scraper-pipeline/lambda_handler.py
+++ scraper-pipeline/scraper-pipeline/lambda_handler.py
@@ -12,17 +12,3 @@ def lambda_handler(event, context=None):
         guardian_rss_feed_urls=event['guardian'],
         express_rss_feed_urls=event['express'],
     ).run()
-
-
-{
-    "guardian": [
-        "https://www.theguardian.com/politics/rss",
-        "https://www.theguardian.com/us-news/us-politics/rss",
-        "https://www.theguardian.com/world/rss"
-    ],
-    "express": [
-        "https://www.express.co.uk/posts/rss/139/politics",
-        "https://www.express.co.uk/posts/rss/198/us",
-        "https://www.express.co.uk/posts/rss/78/world"
-    ]
-}
