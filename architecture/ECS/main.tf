27dfc90d686927e226bc80114561aeec1d42a00a
AntuFTW <antarikshpatel2002@gmail.com>
2025-04-11 18:34:56 +0100

    ECS works with dashboard now

--- architecture/ECS/main.tf
+++ architecture/ECS/main.tf
@@ -67,13 +67,6 @@ resource "aws_security_group" "dashboard_sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
-  ## Check if we need!!!
-  # ingress {
-  #   from_port        = 5432
-  #   to_port          = 5432
-  #   protocol         = "tcp"
-  #   cidr_blocks      = ["0.0.0.0/0"]
-  # }
   
   egress {
     from_port        = 0
