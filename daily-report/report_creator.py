5be62ad868842e4076dd0a3b37d0fcf7ed577c05
Josh Allen <trainee.josh.allen@sigmalabs.co.uk>
2025-04-11 22:04:44 +0100

    outlet sentiment

--- daily-report/report_creator.py
+++ daily-report/report_creator.py
@@ -2,7 +2,7 @@
 Script for extracting insights from the rds and writing them up into a report
 '''
 import os
-from datetime import date
+from datetime import date, timedelta
 from dotenv import load_dotenv
 import plotly.express as px
 import pandas as pd
@@ -13,6 +13,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
+# once we have historic data AND a.article_published_date: : DATE = %s
 SENTIMENT_BY_TOPIC_QUERY = '''
                 WITH guardian_topics_avg AS (SELECT t.topic_name,
                     AVG(at.article_topic_compound_sentiment::numeric) AS compound
@@ -20,7 +21,7 @@ SENTIMENT_BY_TOPIC_QUERY = '''
                     JOIN topic AS t ON t.topic_id = at.topic_id
                     JOIN article AS a ON a.article_id = at.article_id
                     JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-                    WHERE no.news_outlet_name = %s
+                    WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
                     GROUP BY t.topic_name),
 
                 express_topics_avg AS (SELECT t.topic_name,
@@ -29,7 +30,7 @@ SENTIMENT_BY_TOPIC_QUERY = '''
                     JOIN topic AS t ON t.topic_id = at.topic_id
                     JOIN article AS a ON a.article_id = at.article_id
                     JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-                    WHERE no.news_outlet_name = %s
+                    WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
                     GROUP BY t.topic_name)
 
                     SELECT gta.topic_name, ROUND(gta.compound, 2):: float AS guardian_compound, ROUND(eta.compound, 2):: float AS express_compound, ROUND(ABS(gta.compound - eta.compound),2)::float AS compound_diff
@@ -38,38 +39,42 @@ SENTIMENT_BY_TOPIC_QUERY = '''
                     ORDER BY compound_diff DESC
                     ;'''
 
-#    AVG(at.article_topic_positive_sentiment: : numeric) AS positive,
-#             AVG(at.article_topic_negative_sentiment: : numeric) AS negative,
-#             AVG(at.article_topic_neutral_sentiment: : numeric) AS neutral,
+MOST_COVERED_TOPIC_QUERY = '''
+                            WITH topic_counts AS (
+                                SELECT t.topic_name, COUNT(*) AS topic_frequency
+                                FROM article_topic AS at
+                                JOIN topic AS t ON t.topic_id = at.topic_id
+                                JOIN article AS a ON a.article_id = at.article_id
+                                JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
+                                WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
+                                GROUP BY t.topic_name
+                            ),
+                            total_topics AS (
+                                SELECT COUNT(*) AS total_count
+                                FROM article_topic AS at
+                                JOIN article AS a ON a.article_id = at.article_id
+                                JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
+                                WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
+                            )
 
-# ROUND(ABS(gta.positive - eta.positive), 2) AS positive_diff,
-# ROUND(ABS(gta.negative - eta.negative), 2) AS negative_diff,
-# ROUND(ABS(gta.neutral - eta.neutral), 2) AS neutral_diff,
-# AND a.article_published_date: : DATE = %s
-
-MOST_COVERED_TOPIC_QUERY = '''WITH topic_counts AS (
-    SELECT t.topic_name, COUNT(*) AS topic_frequency
-    FROM article_topic AS at
-    JOIN topic AS t ON t.topic_id = at.topic_id
-    JOIN article AS a ON a.article_id = at.article_id
-    JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-    WHERE no.news_outlet_name = %s
-    GROUP BY t.topic_name
-),
-total_topics AS (
-    SELECT COUNT(*) AS total_count
-    FROM article_topic AS at
-    JOIN article AS a ON a.article_id = at.article_id
-    JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-    WHERE no.news_outlet_name = %s
-)
-
-SELECT tc.topic_name,
-       ROUND((tc.topic_frequency * 100.0) / tt.total_count, 2) AS topic_percentage
-FROM topic_counts tc
-JOIN total_topics tt ON true
-ORDER BY topic_percentage DESC;
+                            SELECT tc.topic_name,
+                                ROUND((tc.topic_frequency * 100.0) / tt.total_count, 2) AS topic_percentage
+                            FROM topic_counts tc
+                            JOIN total_topics tt ON true
+                            ORDER BY topic_percentage DESC;
 '''
+
+OUTLET_SENTIMENT_QUERY = '''
+                    SELECT no.news_outlet_name,
+                    ROUND(AVG(at.article_topic_compound_sentiment::numeric), 3)::float AS compound
+                    FROM article_topic as at
+                    JOIN topic AS t ON t.topic_id = at.topic_id
+                    JOIN article AS a ON a.article_id = at.article_id
+                    JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
+                    WHERE a.article_published_date:: DATE = %s
+                    GROUP BY no.news_outlet_name;'''
+
+YESTERDAYS_DATE = date.today()
 
 
 class ReportCreator:
@@ -90,50 +95,68 @@ class ReportCreator:
             port=os.environ["DB_PORT"],
         )
 
-    def _get_guardian_frequent_topic(self):
+# GET METHODS FROM DB
+    def _get_frequent_topic(self, outlet):
         '''Retrieves the average topic sentiments for each outlet'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(MOST_COVERED_TOPIC_QUERY,
-                        ('The Guardian', 'The Guardian'))
+                        (outlet, YESTERDAYS_DATE, outlet, YESTERDAYS_DATE))
             topic_frequency = cur.fetchall()
             return topic_frequency
-
-    def _get_express_frequent_topic(self):
-        '''Retrieves the average topic sentiments for each outlet'''
-        with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            cur.execute(MOST_COVERED_TOPIC_QUERY,
-                        ('Daily Express', 'Daily Express'))
-            topic_frequency = cur.fetchall()
-            print(topic_frequency)
-        return topic_frequency
 
     def _get_difference_in_topic(self):
         '''Retrieves the average topic sentiments for each outlet and the difference between outlets'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             date_now = date.today()
             cur.execute(SENTIMENT_BY_TOPIC_QUERY,
-                        ('The Guardian', 'Daily Express'))
+                        ('The Guardian', YESTERDAYS_DATE, 'Daily Express', YESTERDAYS_DATE))
             difference_in_topic = cur.fetchall()
 
         return difference_in_topic
+
+    def _get_difference_in_outlet(self):
+        '''Retrieves the average topic sentiment between different between outlets'''
+        with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+            previous_date = (YESTERDAYS_DATE -
+                             timedelta(days=1)).strftime('%Y-%m-%d')
+            cur.execute(OUTLET_SENTIMENT_QUERY, (YESTERDAYS_DATE,))
+            yesterdays_scores = cur.fetchall()
+            cur.execute(OUTLET_SENTIMENT_QUERY, (previous_date, ))
+            two_days_scores = cur.fetchall()
+            yesterdays_scores_tool = {
+                score.get('news_outlet_name'): score.get('compound') for score in yesterdays_scores}
+            two_days_scores_tool = {
+                score.get('news_outlet_name'): score.get('compound') for score in two_days_scores}
+
+            increase = '🟢⬆️'
+            decrease = '🔴⬇️'
+
+            express = decrease
+            guard = decrease
+
+            if yesterdays_scores_tool['The Guardian'] > two_days_scores_tool['The Guardian']:
+                guard = increase
+            if yesterdays_scores_tool['Daily Express'] > two_days_scores_tool['Daily Express']:
+                express = increase
+
+            return f"{yesterdays_scores_tool['The Guardian']} {guard}", f"{yesterdays_scores_tool['Daily Express']} {express}"
+
+# GRAPH METHODS
 
     def topics_sentiment_diff_bar_chart(self) -> list[str]:
         '''Returns a bar chart displaying the average topic compound sentiment for each outlet'''
         difference_in_topic = self._get_difference_in_topic()
         difference_in_topic_df = pd.DataFrame(difference_in_topic)
 
-        print(difference_in_topic_df)
-
         fig = px.bar(difference_in_topic_df,
                      x='topic_name',
                      y=['guardian_compound', 'express_compound'],
-                     title='Sentiment Comparison: Guardian vs Express',
+                     title='Average Sentiment per Topic: Guardian vs Express',
                      labels={'topic_name': 'Topic',
-                             'value': 'Compound Sentiment'},
+                             'value': 'Average Sentiment Score'},
                      barmode='group',
                      color_discrete_map={'guardian_compound': 'red', 'express_compound': 'blue'})
         fig.update_layout(
-            title_text="Topic Breakdown: The Guardian vs The Daily Express",
             showlegend=True,
             height=500,  # Increase the height of the entire figure
             width=1000,
@@ -153,45 +176,55 @@ class ReportCreator:
             base_image = b64encode(img_file.read()).decode('utf-8')
         return base_image
 
-    def guardian_topics_freq_bar_chart(self) -> go.Figure:
+    def guardian_topics_freq_pie_chart(self) -> go.Figure:
         '''Returns a pie chart for The Guardian showing frequent topics.'''
-        difference_in_topic = self._get_guardian_frequent_topic()
+        difference_in_topic = self._get_frequent_topic('The Guardian')
         difference_in_topic_df = pd.DataFrame(difference_in_topic)
 
-        # Create the pie chart for The Guardian
         fig_guardian = px.pie(difference_in_topic_df, names='topic_name', values='topic_percentage',
                               title="Breakdown of topics covered by The Guardian yesterday")
 
+        fig_guardian.update_traces(
+            textinfo='label+percent',   # Shows only label and percent
+            insidetextorientation='radial',
+            textposition='inside'
+        )
+
         return fig_guardian
 
-    def express_topics_freq_bar_chart(self) -> go.Figure:
+    def express_topics_freq_pie_chart(self) -> go.Figure:
         '''Returns a pie chart for The Daily Express showing frequent topics.'''
-        difference_in_topic = self._get_express_frequent_topic()
+        difference_in_topic = self._get_frequent_topic('Daily Express')
         difference_in_topic_df = pd.DataFrame(difference_in_topic)
 
-        # Create the pie chart for The Daily Express
         fig_express = px.pie(difference_in_topic_df, names='topic_name', values='topic_percentage',
                              title="Breakdown of topics covered by The Daily Express yesterday")
+
+        fig_express.update_traces(
+            textinfo='label+percent',   # Shows only label and percent
+            insidetextorientation='radial',
+            textposition='inside'
+        )
 
         return fig_express
 
     def combined_pie_charts(self) -> str:
         '''Returns a combined bar chart displaying pie charts for The Guardian and The Daily Express'''
         # Create a subplot layout (2 columns, 1 row)
-    # Define specs to indicate that the subplots should be pie charts
+        # Define specs to indicate that the subplots should be pie charts
         specs = [[{'type': 'pie'}, {'type': 'pie'}]]
 
         # Create subplots with the defined specs for pie charts
         fig = make_subplots(
             rows=1,
             cols=2,
-            subplot_titles=("The Guardian", "The Daily Express"),
+            subplot_titles=("The Guardian", "Daily Express"),
             specs=specs  # Specify that both subplots are for pie charts
         )
 
         # Get both pie charts
-        fig_guardian = self.guardian_topics_freq_bar_chart()
-        fig_express = self.express_topics_freq_bar_chart()
+        fig_guardian = self.guardian_topics_freq_pie_chart()
+        fig_express = self.express_topics_freq_pie_chart()
 
         # Add the pie chart data to the subplots
         for trace in fig_guardian.data:
@@ -202,22 +235,21 @@ class ReportCreator:
 
         # Update layout to adjust spacing and appearance
         fig.update_layout(
-            title_text="Topic Breakdown: The Guardian vs The Daily Express",
+            title_text="Topic Coverage per News Outlet",
             showlegend=True,
-            height=580,  # Increase the height of the entire figure
-            width=1000,
+            height=400,  # Increase the height of the entire figure
+            width=750,
             title_x=0.5,
             legend=dict(
                 orientation="h",  # Horizontal legend
                 yanchor="bottom",  # Align legend to the bottom
-                y=-0.3,  # Place the legend below the chart
+                y=-0.6,  # Place the legend below the chart
                 xanchor="center",  # Center the legend horizontally
                 x=0.5,
                 font=dict(
-                    size=8)  # Center the legend horizontally
+                    size=6)  # Center the legend horizontally
             ),
         )
-
         # Write image to file
         fig.write_image("combined_topics_freq.png")
         with open("combined_topics_freq.png", "rb") as img_file:
@@ -226,21 +258,24 @@ class ReportCreator:
 
     def write_to_html(self):
         """Writes analysis to html"""
-        today_date = date.today()
-        bar_chart = self.topics_sentiment_diff_bar_chart()
+        # METRICS
+        guard_score, express_score = self._get_difference_in_outlet()
+
         polarised_topics = self._get_difference_in_topic()
         diff_topics = [topic.get('topic_name')
                        for topic in polarised_topics[:3]]
         agree_topics = [topic.get('topic_name')
                         for topic in polarised_topics[-3:]]
-        express_freq_topics = self._get_express_frequent_topic()
-        guard_freq_topics = self._get_guardian_frequent_topic()
+        express_freq_topics = self._get_frequent_topic('Daily Express')
+        guard_freq_topics = self._get_frequent_topic('The Guardian')
 
         guard_topics = [topic.get('topic_name')
                         for topic in guard_freq_topics[:3]]
         express_topics = [topic.get('topic_name')
                           for topic in express_freq_topics[:3]]
 
+        # GRAPHS
+        bar_chart = self.topics_sentiment_diff_bar_chart()
         combined_pie = self.combined_pie_charts()
 
         html_string = f"""
@@ -262,6 +297,10 @@ class ReportCreator:
                                         margin: auto;
                                         padding: 20px;
                                     }}
+                                    .sentiment-container{{display: flex;
+                                                    justify-content: space-between;
+                                                    margin-top: 20px;
+                                                }}
                                     h1, h2 {{
                                         color: #333;
                                     }}
@@ -270,10 +309,14 @@ class ReportCreator:
                             </head>
                             <body>
                                 <div class="container">
-                                    <h1>News Polarisation Analysis {today_date}</h1>
+                                    <h1>News Polarisation Analysis {YESTERDAYS_DATE}</h1>
+                                    <div class="sentiment-container">
+                                        <h2>The Guardian's Overall Sentiment: <strong>{guard_score}</strong > </h2 >
+                                        <h2>The Daily Express' Overall Sentiment: <strong>{express_score}</strong></h2>
+                                    </div>
                                     <br>
                                     <h2>Most Polarised Topics Today</h2>
-                                    <p>Today's news coverage from two prominent UK outlets—the left-leaning
+                                    <p>Yesterday's news coverage from two prominent UK outlets—the left-leaning
                                     <em>Guardian</em> and the right-leaning <em>Daily Express</em>—has been analyzed
                                       for potential bias. The analysis revealed significant disparities in perspective
                                         on the following topics: <strong>{diff_topics[0]}</strong>, <strong>{diff_topics[1]}</strong>,
@@ -290,6 +333,8 @@ class ReportCreator:
                                     <strong>{express_topics[2]}</strong> being covered most frequently for the Daily Express</p>
                                     <br>
                                     <img src="data:image/png;base64,{combined_pie}" alt="Polarisation Chart" style="width: 100%;">
+                                    <br>
+                                    <br>
 
 
 
@@ -310,5 +355,6 @@ if __name__ == "__main__":
     report = ReportCreator()
     try:
         report.write_to_html()
+
     finally:
         report.close_connection()
