72f77746c108121f651daf40630c898b9b783828
Josh Allen <trainee.josh.allen@sigmalabs.co.uk>
2025-04-14 16:23:29 +0100

    get methods refactor progress

--- daily-report/report_creator.py
+++ daily-report/report_creator.py
@@ -11,8 +11,14 @@ from psycopg2.extensions import connection
 from base64 import b64encode
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from jinja2 import Environment, FileSystemLoader, Template
-from xhtml2pdf import pisa
+from jinja2 import Environment, FileSystemLoader
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email import encoders
+import boto3
+from weasyprint import HTML
+import base64
 
 
 # once we have historic data AND a.article_published_date: : DATE = %s
@@ -42,28 +48,28 @@ SENTIMENT_BY_TOPIC_QUERY = '''
                     ;'''
 
 MOST_COVERED_TOPIC_QUERY = '''
-                            WITH topic_counts AS (
-                                SELECT t.topic_name, COUNT(*) AS topic_frequency
-                                FROM article_topic AS at
-                                JOIN topic AS t ON t.topic_id = at.topic_id
-                                JOIN article AS a ON a.article_id = at.article_id
-                                JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-                                WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
-                                GROUP BY t.topic_name
-                            ),
-                            total_topics AS (
-                                SELECT COUNT(*) AS total_count
-                                FROM article_topic AS at
-                                JOIN article AS a ON a.article_id = at.article_id
-                                JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
-                                WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
-                            )
+                    WITH topic_counts AS (
+                        SELECT t.topic_name, COUNT(*) AS topic_frequency
+                        FROM article_topic AS at
+                        JOIN topic AS t ON t.topic_id = at.topic_id
+                        JOIN article AS a ON a.article_id = at.article_id
+                        JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
+                        WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
+                        GROUP BY t.topic_name
+                    ),
+                    total_topics AS (
+                        SELECT COUNT(*) AS total_count
+                        FROM article_topic AS at
+                        JOIN article AS a ON a.article_id = at.article_id
+                        JOIN news_outlet AS no ON no.news_outlet_id = a.news_outlet_id
+                        WHERE no.news_outlet_name = %s AND a.article_published_date:: DATE = %s
+                    )
 
-                            SELECT tc.topic_name,
-                                ROUND((tc.topic_frequency * 100.0) / tt.total_count, 2) AS topic_percentage
-                            FROM topic_counts tc
-                            JOIN total_topics tt ON true
-                            ORDER BY topic_percentage DESC;
+                    SELECT tc.topic_name,
+                        ROUND((tc.topic_frequency * 100.0) / tt.total_count, 2) AS topic_percentage
+                    FROM topic_counts tc
+                    JOIN total_topics tt ON true
+                    ORDER BY topic_percentage DESC;
 '''
 
 OUTLET_SENTIMENT_QUERY = '''
@@ -118,7 +124,7 @@ class ReportCreator:
 
 # GET METHODS
     def _get_frequent_topic(self, outlet):
-        '''Retrieves the average topic sentiments for each outlet'''
+        '''Retrieves the percentage that each topic has been covered by the given outlet'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(MOST_COVERED_TOPIC_QUERY,
                         (outlet, YESTERDAYS_DATE, outlet, YESTERDAYS_DATE))
@@ -126,66 +132,62 @@ class ReportCreator:
             return topic_frequency
 
     def _get_difference_in_topic(self):
-        '''Retrieves the average topic sentiments for each outlet and the difference between outlets'''
+        '''Retrieves the difference in average topic sentiment between outlets'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            date_now = date.today()
             cur.execute(SENTIMENT_BY_TOPIC_QUERY,
                         ('The Guardian', YESTERDAYS_DATE, 'Daily Express', YESTERDAYS_DATE))
             difference_in_topic = cur.fetchall()
-
         return difference_in_topic
 
-    def _get_difference_in_outlet(self):
-        '''Retrieves the average topic sentiment between different between outlets'''
+    def _get_outlet_sentiment(self, date_of_interest: date) -> dict[str:str]:
+        '''Returns the average sentiment of each outlet for a given date'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            previous_date = (YESTERDAYS_DATE -
-                             timedelta(days=1)).strftime('%Y-%m-%d')
-            cur.execute(OUTLET_SENTIMENT_QUERY, (YESTERDAYS_DATE,))
-            yesterdays_scores = cur.fetchall()
-            cur.execute(OUTLET_SENTIMENT_QUERY, (previous_date, ))
-            two_days_scores = cur.fetchall()
-            yesterdays_scores_tool = {
-                score.get('news_outlet_name'): score.get('compound') for score in yesterdays_scores}
-            two_days_scores_tool = {
-                score.get('news_outlet_name'): score.get('compound') for score in two_days_scores}
+            cur.execute(OUTLET_SENTIMENT_QUERY, (date_of_interest,))
+            sentiment_score = cur.fetchall()
+        return {score.get('news_outlet_name'): score.get('compound') for score in sentiment_score}
 
-            increase = '🟢⬆️'
-            decrease = '🔴⬇️'
+    def _get_difference_in_outlet(self) -> tuple[str:str]:
+        '''Returns the average sentiment score for each day and whether this has changed since the previous day'''
+        increase = '🟢⬆️'
+        decrease = '🔴⬇️'
+        equal = '🟡⏸️'
+        express_change = equal
+        guard_change = equal
 
-            express = decrease
-            guard = decrease
+        previous_date = (YESTERDAYS_DATE - timedelta(days=1)
+                         ).strftime('%Y-%m-%d')
+        yesterdays_sentiment_scores = self._get_outlet_sentiment(
+            YESTERDAYS_DATE)
+        two_days_sentiment_score = self._get_outlet_sentiment(previous_date)
 
-            if yesterdays_scores_tool['The Guardian'] > two_days_scores_tool['The Guardian']:
-                guard = increase
-            if yesterdays_scores_tool['Daily Express'] > two_days_scores_tool['Daily Express']:
-                express = increase
+        if yesterdays_sentiment_scores['The Guardian'] > two_days_sentiment_score['The Guardian']:
+            guard_change = increase
 
-            return f"{yesterdays_scores_tool['The Guardian']} {guard}", f"{yesterdays_scores_tool['Daily Express']} {express}"
+        if yesterdays_sentiment_scores['The Guardian'] < two_days_sentiment_score['The Guardian']:
+            guard_change = decrease
 
-    def guard_articles(self):
-        '''Finds the top polarising articles for the guardian'''
+        if yesterdays_sentiment_scores['Daily Express'] > two_days_sentiment_score['Daily Express']:
+            express_change = increase
+
+        if yesterdays_sentiment_scores['Daily Express'] < two_days_sentiment_score['Daily Express']:
+            express_change = decrease
+
+        return f"{yesterdays_sentiment_scores['The Guardian']} {guard_change}", f"{yesterdays_sentiment_scores['Daily Express']} {express_change}"
+
+    def top_articles(self, outlet: str) -> tuple[list[dict], list[dict]]:
+        '''Finds the top polarising articles for the given outlet'''
         with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
             cur.execute(TOP_NEGATIVE_ARTICLES,
-                        (YESTERDAYS_DATE, 'The Guardian'))
+                        (YESTERDAYS_DATE, outlet))
             guardian_positives = cur.fetchall()
             cur.execute(TOP_POSITIVE_ARTICLES,
-                        (YESTERDAYS_DATE, 'The Guardian'))
+                        (YESTERDAYS_DATE, outlet))
             guardian_negatives = cur.fetchall()
             return guardian_positives, guardian_negatives
 
-    def express_articles(self):
-        '''Finds the top polarising articles for daily express'''
-        with self.__connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
-            cur.execute(TOP_NEGATIVE_ARTICLES,
-                        (YESTERDAYS_DATE, 'Daily Express'))
-            express_positives = cur.fetchall()
-            cur.execute(TOP_POSITIVE_ARTICLES,
-                        (YESTERDAYS_DATE, 'Daily Express'))
-            express_negatives = cur.fetchall()
-            return express_positives, express_negatives
-
 
 # GRAPH METHODS
+
 
     def topics_sentiment_diff_bar_chart(self) -> list[str]:
         '''Returns a bar chart displaying the average topic compound sentiment for each outlet'''
@@ -202,21 +204,20 @@ class ReportCreator:
                      color_discrete_map={'guardian_compound': 'red', 'express_compound': 'blue'})
         fig.update_layout(
             showlegend=True,
-            height=500,  # Increase the height of the entire figure
-            width=1000,
+            autosize=True,
             title_x=0.5,
             legend=dict(
                 orientation="h",  # Horizontal legend
                 yanchor="bottom",  # Align legend to the bottom
-                y=-0.5,  # Place the legend below the chart
+                y=-0.8,  # Place the legend below the chart
                 xanchor="center",  # Center the legend horizontally
                 x=0.5,
                 font=dict(
                     size=8)  # Center the legend horizontally
             ),)
 
-        fig.write_image("topics_compound.png")
-        with open("topics_compound.png", "rb") as img_file:
+        fig.write_image("/tmp/topics_compound.png")
+        with open("/tmp/topics_compound.png", "rb") as img_file:
             base_image = b64encode(img_file.read()).decode('utf-8')
         return base_image
 
@@ -278,8 +279,7 @@ class ReportCreator:
         fig.update_layout(
             title_text="Topic Coverage per News Outlet",
             showlegend=True,
-            height=400,
-            width=750,
+            autosize=True,
             title_x=0.5,
             legend=dict(
                 orientation="h",
@@ -292,8 +292,8 @@ class ReportCreator:
             ),
         )
 
-        fig.write_image("combined_topics_freq.png")
-        with open("combined_topics_freq.png", "rb") as img_file:
+        fig.write_image("/tmp/combined_topics_freq.png")
+        with open("/tmp/combined_topics_freq.png", "rb") as img_file:
             base_image = b64encode(img_file.read()).decode('utf-8')
         return base_image
 
@@ -319,8 +319,8 @@ class ReportCreator:
                           for topic in express_freq_topics[:3]]
 
         # Polarising articles
-        guardian_pos_art, guardian_neg_art = self.guard_articles()
-        express_pos_art, express_neg_art = self.express_articles()
+        guardian_pos_art, guardian_neg_art = self.top_articles('The Guardian')
+        express_pos_art, express_neg_art = self.top_articles('Daily Express')
 
         # Generate charts (base64-encoded images)
         bar_chart = self.topics_sentiment_diff_bar_chart()
@@ -349,21 +349,92 @@ class ReportCreator:
     def generate_jinja_env(self):
         '''Generates an jinja2 environment and exports it to a html file'''
         jinja_env = Environment(loader=FileSystemLoader('template'))
-        template = jinja_env.get_template('report.html')
+        template = jinja_env.get_template('jinja_template.html')
         context = self.generate_report_context()
         rendered_html = template.render(context)
-        with open('output.html', 'w') as f:
+        # return rendered_html
+        with open('/tmp/report.html', 'w') as f:
             f.write(rendered_html)
+        HTML('/tmp/report.html').write_pdf('/tmp/report.pdf')
+        with open('/tmp/report.pdf', "rb") as pdf_file:
+            pdf = b64encode(pdf_file.read())
+        return pdf
+
+    # def email_generator(self):
+    #     pdf_data = self.generate_jinja_env()
+
+    #     from_email = "trainee.josh.allen@sigmalabs.co.uk"
+    #     to_emails = [
+    #         "trainee.antariksh.patel@sigmalabs.co.uk",
+    #         "trainee.joseph.sasaki@sigmalabs.co.uk",
+    #         "trainee.josh.allen@sigmalabs.co.uk",
+    #         "trainee.jake.hussey@sigmalabs.co.uk"
+    #     ]
+
+    #     # Set up the MIME message
+    #     msg = MIMEMultipart()
+    #     msg['From'] = from_email
+    #     # Don't list all addresses here
+    #     msg['To'] = "trainee.josh.allen@sigmalabs.co.uk"
+    #     msg['Subject'] = "Hello from SES"
+
+    #     # Email body
+    #     body = MIMEText("Today's daily report", 'html')
+    #     msg.attach(body)
+
+    #     # PDF attachment (raw, not base64 upfront!)
+    #     part = MIMEApplication(pdf_data)
+    #     part.add_header('Content-Disposition', 'attachment',
+    #                     filename="document.pdf")
+    #     msg.attach(part)
+
+    #     # Return raw bytes, NOT base64 encoded
+    #     return msg.as_bytes()
+
+    # def send_email(self):
+    #     raw_email_bytes = self.email_generator()
+
+    #     # Create a boto3 client for SES
+    #     # Replace with your SES region
+    #     ses_client = boto3.client('ses', region_name='eu-west-2')
+
+    #     response = ses_client.send_raw_email(
+    #         RawMessage={
+    #             'Data': raw_email_bytes
+    #         },
+    #         Source="trainee.josh.allen@sigmalabs.co.uk",
+    #         Destinations=[
+    #             "trainee.antariksh.patel@sigmalabs.co.uk",
+    #             "trainee.joseph.sasaki@sigmalabs.co.uk",
+    #             "trainee.josh.allen@sigmalabs.co.uk",
+    #             "trainee.jake.hussey@sigmalabs.co.uk"
+    #         ]
+
+    #     )
+    #     print("Email sent! Message ID:", response['MessageId'])
 
     def close_connection(self):
         '''Closes the database connection'''
         self.__connection.close()
 
 
-if __name__ == "__main__":
+def lambda_handler(event, context):
+    '''Lambda function handler'''
     report = ReportCreator()
     try:
-        report.generate_jinja_env()
+        report.send_email()
+        return {'statusCode': 200,
+                'body': 'success'}
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'body': f"Error sending email: {str(e)}"
+        }
 
     finally:
         report.close_connection()
+
+
+if __name__ == "__main__":
+    print(lambda_handler([1, 2, 3], [1, 2, 3]))
