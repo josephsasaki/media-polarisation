3b542752fe74a5b1ff593d7ecb5b800aad66c1aa
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-15 10:49:48 +0100

    Restructured files

---
+++ dashboard/dashboard.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+if __name__ == "__main__":
+    pg = st.navigation([
+        st.Page("page1.py", title="First page", icon="🔥"),
+        st.Page("page2.py", title="Second page", icon="🔥"),
+        st.Page("page3.py", title="Third page", icon="🔥"),
+        st.Page("page4.py", title="Fourth page", icon="🔥"),
+    ])
+    pg.run()
