8c7abcab84eaca7ef3422cf3d2617679f0599c32
josephsasaki <joseph.sasaki.314@gmail.com>
2025-04-16 12:03:09 +0100

    Added page icons

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -7,10 +7,15 @@ import streamlit as st
 
 if __name__ == "__main__":
     pg = st.navigation([
-        st.Page("welcome.py", title="Welcome", icon="🔥"),
-        st.Page("page1.py", title="First page", icon="🔥"),
-        st.Page("page2.py", title="Second page", icon="🔥"),
-        st.Page("page3.py", title="Third page", icon="🔥"),
-        st.Page("page4.py", title="Fourth page", icon="🔥"),
+        st.Page("welcome.py", title="Welcome",
+                icon=":material/home:"),
+        st.Page("page1.py", title="Topic Polarisation",
+                icon=":material/compare_arrows:"),
+        st.Page("page2.py", title="Article Extremes",
+                icon=":material/e911_emergency:"),
+        st.Page("page3.py", title="News Outlet Sentiment",
+                icon=":material/newspaper:"),
+        st.Page("page4.py", title="Topic Sentiment",
+                icon=":material/tag:"),
     ])
     pg.run()
