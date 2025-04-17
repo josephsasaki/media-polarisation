33285dab99eeff0c73e3db490587212156201468
AntuFTW <antarikshpatel2002@gmail.com>
2025-04-17 10:50:07 +0100

    completed bash script for weasyprint activation

---
+++ daily-report/weasyprint_activate.sh
@@ -0,0 +1,4 @@
+export DYLD_FALLBACK_LIBRARY_PATH=/opt/homebrew/lib:$DYLD_FALLBACK_LIBRARY_PATH
+brew install weasyprint
+brew install pkg-config cairo cmake
+brew install pango cairo gdk-pixbuf libffi
